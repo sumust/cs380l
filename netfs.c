9b16922c673141abc466417f306445264fc4e20c
sumust <96579169+sumust@users.noreply.github.com>
2023-10-25 17:57:13 -0500

    Fixed netfs.c

--- netfs.c
+++ netfs.c
@@ -7,13 +7,7 @@
 
 static const char *remote_server = NULL;
 
-char *get_temp_filename(const char *path) {
-    char *temp = malloc(strlen("/tmp/netfs_") + strlen(path) + 1);
-    sprintf(temp, "/tmp/netfs_%s", path + 1); // path + 1 to skip the initial '/'
-    return temp;
-}
-
-int run_system_command(const char *cmd) {
+static int run_cmd(const char *cmd) {
     int res = system(cmd);
     if (res != 0) {
         return -EIO; // Input/output error
@@ -22,43 +16,31 @@ int run_system_command(const char *cmd) {
 }
 
 static int netfs_getattr(const char *path, struct stat *stbuf) {
-    int res;
-    char *temp_filename = get_temp_filename(path);
-    char cmd[1024 + strlen(temp_filename)];
+    char cmd[1024];
 
-    snprintf(cmd, sizeof(cmd), "scp %s:%s %s", remote_server, path, temp_filename);
-
-    res = run_system_command(cmd);
+    snprintf(cmd, sizeof(cmd), "scp %s:%s /tmp/netfs_tempfile", remote_server, path);
+    int res = run_cmd(cmd);
     if (res != 0) {
-        free(temp_filename);
         return res;
     }
 
-    res = lstat(temp_filename, stbuf);
-    free(temp_filename);
-
-    if (res == -1) {
-        return -errno;
-    }
-    return 0;
+    return lstat("/tmp/netfs_tempfile", stbuf);
 }
 
 static int netfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
     FILE *f;
-    int res;
-    char *temp_filename = get_temp_filename(path);
-    char cmd[1024 + strlen(temp_filename)];
+    char cmd[1024];
 
-    snprintf(cmd, sizeof(cmd), "scp %s:%s %s", remote_server, path, temp_filename);
-    res = run_system_command(cmd);
+    (void) fi;
+
+    snprintf(cmd, sizeof(cmd), "scp %s:%s /tmp/netfs_tempfile", remote_server, path);
+    int res = run_cmd(cmd);
     if (res != 0) {
-        free(temp_filename);
         return res;
     }
 
-    f = fopen(temp_filename, "rb");
+    f = fopen("/tmp/netfs_tempfile", "rb");
     if (f == NULL) {
-        free(temp_filename);
         return -errno;
     }
 
@@ -66,30 +48,23 @@ static int netfs_read(const char *path, char *buf, size_t size, off_t offset, st
     res = fread(buf, 1, size, f);
     fclose(f);
 
-    free(temp_filename);
-
-    if (res == -1) {
-        return -errno;
-    }
     return res;
 }
 
 static int netfs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
     FILE *f;
-    int res;
-    char *temp_filename = get_temp_filename(path);
-    char cmd[1024 + strlen(temp_filename)];
+    char cmd[1024];
 
-    snprintf(cmd, sizeof(cmd), "scp %s:%s %s", remote_server, path, temp_filename);
-    res = run_system_command(cmd);
+    (void) fi;
+
+    snprintf(cmd, sizeof(cmd), "scp %s:%s /tmp/netfs_tempfile", remote_server, path);
+    int res = run_cmd(cmd);
     if (res != 0) {
-        free(temp_filename);
         return res;
     }
 
-    f = fopen(temp_filename, "r+b");
+    f = fopen("/tmp/netfs_tempfile", "r+b");
     if (f == NULL) {
-        free(temp_filename);
         return -errno;
     }
 
@@ -97,31 +72,28 @@ static int netfs_write(const char *path, const char *buf, size_t size, off_t off
     res = fwrite(buf, 1, size, f);
     fclose(f);
 
-    if (res == -1) {
-        free(temp_filename);
-        return -errno;
-    }
-
-    snprintf(cmd, sizeof(cmd), "scp %s %s:%s", temp_filename, remote_server, path);
-    res = run_system_command(cmd);
-
-    free(temp_filename);
-
+    snprintf(cmd, sizeof(cmd), "scp /tmp/netfs_tempfile %s:%s", remote_server, path);
+    res = run_cmd(cmd);
     if (res != 0) {
         return res;
     }
+
     return res;
 }
 
+static struct fuse_operations netfs_oper = {
+    .getattr    = netfs_getattr,
+    .read       = netfs_read,
+    .write      = netfs_write,
+};
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s <mountpoint> <server>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <server> <mountpoint>\n", argv[0]);
         return 1;
     }
 
-    remote_server = argv[argc - 1];
-    argv[argc - 1] = NULL;
-    argc--;
-
-    return fuse_main(argc, argv, &netfs_oper, NULL);
+    remote_server = argv[1];
+    argv[1] = argv[0]; 
+    return fuse_main(argc - 1, &argv[1], &netfs_oper, NULL);
 }
