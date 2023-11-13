16960579d25479c62559cbb0ef54536e8850bcc0
sumust <96579169+sumust@users.noreply.github.com>
2023-11-12 21:22:43 -0600

    Update test_program1.c

--- test_program1.c
+++ test_program1.c
@@ -1,13 +1,12 @@
-// test_program1.c
 #include <stdio.h>
 
+long fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
-    printf("Test Program 1 is running...\n");
-    // Some computation
-    int sum = 0;
-    for (int i = 0; i < 1000000; i++) {
-        sum += i;
-    }
-    printf("Sum is %d\n", sum);
+    int n = 40; // High enough to cause a considerable calculation time
+    printf("Fibonacci of %d is %ld\n", n, fibonacci(n));
     return 0;
 }
